aaa04976795566ecacbf54b402247400c82b61dc
Jake Allen <jake@hintsy.co>
2021-05-07 15:12:52 -0700

    Changed price upper bound to 28

--- src/index.js
+++ src/index.js
@@ -125,7 +125,7 @@ const makePurchase = (objectId) => {
     if (parseFloat(data.swaps.length) > 26) return
     if (parseFloat(swapsAvail) > 26) return
     // Don't collect if price isn't 25 XTZ or below
-    if (parseFloat(swapAmount) > (26 * 1000000)) return
+    if (parseFloat(swapAmount) > (28 * 1000000)) return
     // This is the cheapest swap, now execute
     collect(swapId, swapAmount)
   })
