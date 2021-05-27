595021c58eb061d0025adf07ddfafb821ef400dc
Jake Allen <jake@hintsy.co>
2021-05-27 09:24:13 -0700

    Added DAI constant.

--- src/constants/tokenAddresses.js
+++ src/constants/tokenAddresses.js
@@ -4,3 +4,4 @@
 
 export const wethAddress = '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619'
 export const usdcAddress = '0x2791bca1f2de4661ed88a30c99a7a9449aa84174'
+export const daiAddress = '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063'
