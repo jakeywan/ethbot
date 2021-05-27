e6772abfb51a7e02ceef281368b75aed2b14791c
Jake Allen <jake@hintsy.co>
2021-05-27 09:24:22 -0700

    Changed Matic URL (other one was failing).

--- src/matic/matic.js
+++ src/matic/matic.js
@@ -5,7 +5,7 @@ dotenv.config()
 // Matic network urls:
 // https://docs.matic.network/docs/develop/network-details/network/
 export const provider =
-  new ethers.providers.JsonRpcProvider('https://rpc-mainnet.maticvigil.com')
+  new ethers.providers.JsonRpcProvider('https://rpc-mainnet.matic.quiknode.pro')
 
 // This key is for demo wallet address: 0xc6ab04C59A14cD50c5d0BDA790dCeFD2484901cE
 export const wallet = new ethers.Wallet(process.env.WALLET_KEY, provider)
