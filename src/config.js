fef40fe87fd7f2635520cadc5b0cf42cadd2d10c
Jake Allen <jake@hintsy.co>
2021-05-30 23:43:36 -0700

    Added script to run locally, but with production config. Updated to latest contract.

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export let contractAddress
 
 if (process.env.NODE_ENV === 'production') {
   nodeUrl = 'https://polygon-mainnet.infura.io/v3/c0c9dead82ea4b22a4da82089006c15d'
-  contractAddress = '0x4f0522cD0cc91c3B4eF30962Ce62790633306d82'
+  contractAddress = '0x5Fd1615aC53051Fa3Db00a6472ed5e5fC84e4e8f'
 } else if (process.env.NODE_ENV === 'development') {
   // local variables
   nodeUrl = 'http://127.0.0.1:8545/'
